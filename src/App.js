a586ea63150e0aafa60bc53d8cf0c105c9530f89
captain-xu <jian.xu@aihuishou.com>
2019-03-30 16:30:28 +0800

    type: feature desc:

--- src/App.js
+++ src/App.js
@@ -2,15 +2,16 @@ import React, { Component } from 'react';
 import './App.scss';
 import 'antd/dist/antd.css';
 import Router from './router'
+import {Header} from '~/components'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header>
-          相聚一堂
-        </header>
-        {Router}
+        <Header />
+        <div className="content">
+          {Router}
+        </div>
       </div>
     );
   }
