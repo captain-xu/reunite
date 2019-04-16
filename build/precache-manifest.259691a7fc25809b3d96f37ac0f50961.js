e3d350b5b0deab92e973194276813fd8c4512816
captain-xu <jian.xu@aihuishou.com>
2019-04-16 10:17:26 +0800

    type: feature desc: build

---
+++ build/precache-manifest.259691a7fc25809b3d96f37ac0f50961.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "f7180b837e94ff490551",
+    "url": "/static/js/runtime~main.f7180b83.js"
+  },
+  {
+    "revision": "1e1bb501083ddb550e07",
+    "url": "/static/js/main.1e1bb501.chunk.js"
+  },
+  {
+    "revision": "b5526465a1c4efcd90f0",
+    "url": "/static/js/3.b5526465.chunk.js"
+  },
+  {
+    "revision": "2dd47b76d5114961f273",
+    "url": "/static/js/2.2dd47b76.chunk.js"
+  },
+  {
+    "revision": "1e1bb501083ddb550e07",
+    "url": "/static/css/main.f730dec1.chunk.css"
+  },
+  {
+    "revision": "2dd47b76d5114961f273",
+    "url": "/static/css/2.77da5c67.chunk.css"
+  },
+  {
+    "revision": "d4a1bd38c5d652bff05aa5f3e6bfc3d4",
+    "url": "/index.html"
+  }
+];
